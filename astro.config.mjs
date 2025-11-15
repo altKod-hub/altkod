845abff8c73155167f89aed3d991c647ecc6d9b8
altKod-hub <info@altkod.com>
2025-11-16 00:19:07 +0100

    Blog Kartice u Izradi

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,16 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import tailwindcss from '@tailwindcss/vite';
+
+import alpinejs from '@astrojs/alpinejs';
+
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: 'https://altkod.com',
+  vite: {
+    plugins: [tailwindcss()]
+  },
+
+  integrations: [alpinejs()]
+});
---
+++ src/data/global.ts
@@ -0,0 +1,39 @@
+// Global Site Data
+const currentYear = new Date().getFullYear();
+export const siteData = {
+    name: "altKod",
+    lang: "hr-HR",
+    ogLang: "hr_HR",
+    langSchema: "hr",
+    copyright: `altKod - ${currentYear} | Sva Prava Pridržana`,
+    ver: "0.0.1",
+    url: "https://altkod.com",
+    cmsURL: "https://altkod.apollo.filipsteficar.com"
+}
+// Define the MenuItem Interface
+interface MenuItem {
+    label: string
+    href: string
+    isExternal?: boolean
+}
+// Main Menu
+export const mainMenu: MenuItem[] = [
+    {
+        label: "Početna",
+        href: "/"
+    },
+    {
+        label: "Centar Znanja",
+        href: "/centar-znanja"
+    }
+]
+// Contact Info
+export const contactInfo = {
+    email: "info@altkod.com",
+    address: "Gornja Reka 103, 10450 Jastrebarsko, Croatia"
+}
+// Social Media
+export const socialMedia = {
+    instagram: "https://instagram.com",
+    youtube: "https://youtube.com"
+}
